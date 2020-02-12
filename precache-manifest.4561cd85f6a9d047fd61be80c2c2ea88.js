64cd35ce4d0fa650d982bb719d8993eb7a007221
Denys <denys.cheporniuk@gmail.com>
2020-02-12 12:45:47 +0200

    Updates

---
+++ precache-manifest.4561cd85f6a9d047fd61be80c2c2ea88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130dd0f521c2a6b1304df38c8ced2eec",
+    "url": "/F-developer_layout/index.html"
+  },
+  {
+    "revision": "86657248a04146ab62f1",
+    "url": "/F-developer_layout/static/css/main.e5869600.chunk.css"
+  },
+  {
+    "revision": "b097fc4133e5b4500696",
+    "url": "/F-developer_layout/static/js/2.575b7b3e.chunk.js"
+  },
+  {
+    "revision": "86657248a04146ab62f1",
+    "url": "/F-developer_layout/static/js/main.585a3f89.chunk.js"
+  },
+  {
+    "revision": "a28897f59eae3a8d063c",
+    "url": "/F-developer_layout/static/js/runtime~main.67fddbb8.js"
+  }
+]);
